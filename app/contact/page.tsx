f3cf4cdfa5c860912fa1cf16413d5fbab4b6e55a
zohaib789 <bzohaib754@gmail.com>
2025-10-15 20:14:34 +0500

    error resove at contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -56,7 +56,7 @@ const Contact = () => {
           <header className="mb-6 text-center">
             <h2 className="text-3xl font-extrabold text-green-800">Contact Us</h2>
             <p className="mt-2 text-sm text-gray-600">
-              Have feedback or a suggestion? Send us a message and we'll get back to you.
+              Have feedback or a suggestion? Send us a message and we will get back to you.
             </p>
           </header>
 
