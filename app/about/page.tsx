37614813eac23f8f11f4b0861d5d31af90389462
AnooshaNadeem12345 <anooshanadeem226@gmail.com>
2025-10-15 21:52:23 +0500

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 import React from 'react'
 import { motion } from 'framer-motion';
-import { div } from 'framer-motion/client';
-import { once } from 'events';
 import Image from 'next/image';
 
 
